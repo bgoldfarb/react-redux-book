bf9e99158866a7b97d8ae4ac12998080002b6295
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-13 12:18:38 -0400

    Create the active book reducer that produced new app state whenever the book selected action was triggered

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books'
+import ActiveBook from './reducer_active_book'
 
+
+//keys passed in to combineReducers ends up as a key in the global state
 const rootReducer = combineReducers({
-  books: BooksReducer 
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
