f482b216da4596b363e417681f1a250415879004
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-13 12:01:06 -0400

    Creating first action and setting up bindActionCreators

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react'
 //Glue between react and redux
 import { connect } from 'react-redux'
 
+import { selectBook } from '../actions/index'
 
+//Makes sure out actions flow to reducers
+import { bindActionCreators } from 'redux'
 
 //Cares about when the list of books changes
 class BookList extends Component{
@@ -31,6 +34,13 @@ function mapStateToProps(state){
     }
 }
 
+//Anything returned from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch){
+    //Whenever selectBook is called, the result should be passed to all of
+    //our reducers
+    return bindActionCreators({selectBook : selectBook}, dispatch)
+}
+
 //Connect takes a function and a component and produces a container
 //Container is a component that is aware of state in redux
-export default connect(mapStateToProps)(BookList)
+export default connect(mapStateToProps, mapDispatchToProps)(BookList)
