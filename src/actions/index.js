08c366ed0575c8d3134b3dc6efd76576e2df2f9f
bgoldfarb <briangoldfarb@brians-mbp.attlocal.net>
2018-05-13 12:10:13 -0400

    Creating the action creator and adding payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,9 @@
 export function selectBook(book) {
-    console.log('A Book has been selected: ', book.title)
+    //selectBook is an action creator, it needs to return an action
+    //an action is an object with a type property and sometimes a payload
+    return {
+        type: 'BOOK_SELECTED',
+        payload: book
+    }
 }
 
